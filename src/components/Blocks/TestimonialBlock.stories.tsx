0d3acd104e338d4adc159b6c93e976ca34b41d8a
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-06-14 16:57:57 +0100

    fix: wrong types in testimonial stories

--- src/components/Blocks/TestimonialBlock.stories.tsx
+++ src/components/Blocks/TestimonialBlock.stories.tsx
@@ -7,13 +7,13 @@ import { StoryFixtures } from '../../types/utils'
 export default {
   title: 'Blocks/Testimonial Block',
   component: TestimonialBlock,
-} as ComponentMeta<typeof testimonialBlock>
+} as ComponentMeta<typeof TestimonialBlock>
 
 export const Default: ComponentStory<typeof TestimonialBlock> = () => (
   <TestimonialBlock {...fixtures.default} />
 )
 
-const fixtures: StoryFixtures<typeof NetZeroBlock> = {
+const fixtures: StoryFixtures<typeof TestimonialBlock> = {
   default: {
     content: `“The Climate Agency is changing the world by impacting the way people think. Sophie and Lisa were crucial to helping us express ourselves better.”`,
     signature: `Neri Oxman, Director of Research`,
