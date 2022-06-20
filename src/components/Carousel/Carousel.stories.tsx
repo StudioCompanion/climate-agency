8c8fe612e556264540daa984e3310a432fe2ecb8
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-06-20 11:29:36 +0100

    feat: add carousel component

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -3,6 +3,7 @@ import { Carousel } from './Carousel'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 import { StoryFixtures } from '../../types/utils'
+import { TextTestimonial } from 'components/Text/TextTestimonial'
 
 export default {
   title: 'Carousel/Carousel',
@@ -15,29 +16,23 @@ export const Default: ComponentStory<typeof Carousel> = () => (
 
 const fixtures: StoryFixtures<typeof Carousel> = {
   default: {
+    children: (slide: any) => <TextTestimonial {...slide} />,
     slides: [
       {
+        key: 'Neri Oxman',
         content: `“The Climate Agency is changing the world by impacting the way people think. Sophie and Lisa were crucial to helping us express ourselves better.”`,
         signature: `Neri Oxman, Director of Research`,
         organisation: `Direct Air Capture Coalition`,
       },
       {
+        key: 'Joe Smith',
         content: `“This is the 2nd testimonial”`,
         signature: `Joe Smith, Director of Research`,
         organisation: `Testing`,
       },
       {
+        key: 'Mary Jones',
         content: `“This is the 3rd testimonial”`,
-        signature: `Mary Jones, Director of Research`,
-        organisation: `Testing 2`,
-      },
-      {
-        content: `“This is the 4th testimonial”`,
-        signature: `Joe Smith, Director of Research`,
-        organisation: `Testing`,
-      },
-      {
-        content: `“This is the 5th testimonial”`,
         signature: `Mary Jones, Director of Research`,
         organisation: `Testing 2`,
       },
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,69 +1,202 @@
+import { useEffect, useState, useRef } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
+import clsx from 'clsx'
 
 import { styled } from 'styles/stitches.config'
 
-interface CarouselProps {
-  //   options?: EmblaOptionsType
+import { useReducedMotion } from 'hooks/useReducedMotion'
+import { useIsomorphicLayoutEffect } from 'hooks/useIsomorphicEffect'
+
+interface BaseItem {
+  key: string
+  [key: string]: unknown
+}
+
+interface CarouselProps<Item extends BaseItem> {
+  slides: readonly Item[]
+  children: (render: Item) => JSX.Element
   className?: string
 }
 
-export const Carousel = ({ className }: CarouselProps) => {
-  const options = { delay: 2000 }
-  const autoplayRoot = (emblaRoot: HTMLElement) => emblaRoot.parentElement
-  const autoplay = Autoplay(options, autoplayRoot)
+export const Carousel = <Item extends BaseItem>({
+  className,
+  slides,
+  children,
+}: CarouselProps<Item>) => {
+  /**
+   * The active index of the carousel
+   * indexed at 0
+   */
+  const [activeIndex, setActiveIndex] = useState(0)
 
-  const [viewportRef, embla] = useEmblaCarousel({ loop: false }, [autoplay])
+  /**
+   * Wrap this in useState so it's never recreated
+   * And don't play on init because some people
+   * prefer reduced motion
+   */
+  const [carouselAutoplay] = useState(() =>
+    Autoplay({
+      playOnInit: false,
+    })
+  )
+  const [viewportRef, embla] = useEmblaCarousel(
+    {
+      loop: true,
+    },
+    [carouselAutoplay]
+  )
+
+  const reducedMotion = useReducedMotion()
+
+  /**
+   * Allow user preferences to dictate autoplay
+   */
+  useIsomorphicLayoutEffect(() => {
+    if (reducedMotion) {
+      carouselAutoplay.stop()
+    } else {
+      carouselAutoplay.play()
+    }
+  }, [reducedMotion])
+
+  /**
+   * When a carousel button has been clicked the relevant
+   * slide should be focussed according to accessibility
+   * guidelines. We therefore collect all the refs of the
+   * slides so we can focus them as and when
+   */
+  const slideRefs = useRef<HTMLLIElement[]>([])
+
+  const handleDotClick = (index: number) => () => {
+    if (embla) {
+      embla.scrollTo(index)
+      setActiveIndex(index)
+
+      if (slideRefs.current[index]) {
+        slideRefs.current[index].focus()
+      }
+    }
+  }
+
+  /**
+   * Standard embla handler to record the activeIndex
+   */
+  useEffect(() => {
+    const handleSelect = () => {
+      if (embla) {
+        setActiveIndex(embla.selectedScrollSnap())
+      }
+    }
+
+    if (embla) {
+      embla.on('select', handleSelect)
+    }
+
+    return () => {
+      if (embla) {
+        embla.off('select', handleSelect)
+      }
+    }
+  }, [embla])
 
   return (
-    <CarouselWrap className={className}>
-      <ViewportWrap ref={viewportRef}>
-        <ContainerWrap>
-          {/* {slides &&
-            slides.map((item, idx) => (
-              <SlideWrap key={idx}>
-                <SlideInnerWrap>
-                  <TestimonialBlock
-                    content={item.content}
-                    signature={item.signature}
-                    organisation={item.organisation}
-                  />
-                </SlideInnerWrap>
-              </SlideWrap>
-            ))} */}
-        </ContainerWrap>
+    <CarouselWrap className={clsx('embla', className)}>
+      <ViewportWrap className="embla__viewport" ref={viewportRef}>
+        <SlidesContainer className="embla__container">
+          {slides &&
+            slides.map((item, i) => (
+              <Slide
+                ref={(ref) => (slideRefs.current[i] = ref!)}
+                key={item.key}
+                className="embla__slide"
+                tabIndex={-1}
+              >
+                {children(item)}
+              </Slide>
+            ))}
+        </SlidesContainer>
       </ViewportWrap>
+      <CarouselFooter>
+        {/**
+         * This is a live region that can read out
+         * the active slide to screen readers
+         */}
+        <LiveRegion aria-live="polite" aria-atomic="true">{`Slide ${
+          activeIndex + 1
+        } of ${slides.length}`}</LiveRegion>
+        {new Array(slides.length).fill(null).map((_, index) => (
+          <Dot
+            key={index}
+            type="button"
+            style={{ opacity: activeIndex === index ? 1 : 0.4 }}
+            onClick={handleDotClick(index)}
+          >
+            {/**
+             * Buttons require a label, although we don't
+             * need to explicitly have the lable showing
+             */}
+            <VisuallyHiddenLabel>{`Image ${index + 1}`}</VisuallyHiddenLabel>
+            {activeIndex === index ? (
+              <VisuallyHiddenLabel>{`(current slide)`}</VisuallyHiddenLabel>
+            ) : null}
+          </Dot>
+        ))}
+      </CarouselFooter>
     </CarouselWrap>
   )
 }
 
 const CarouselWrap = styled('div', {
-  backgroundColor: 'transparent',
-  p: '20px',
-  mx: 'auto',
+  width: '100%',
+  position: 'relative',
 })
 
 const ViewportWrap = styled('div', {
+  width: '100%',
+  position: 'relative',
   overflow: 'hidden',
 })
 
-const ContainerWrap = styled('div', {
+const SlidesContainer = styled('ul', {
   display: 'flex',
   userSelect: 'none',
-  ml: '-10px',
 })
 
-const SlideWrap = styled('div', {
+const Slide = styled('li', {
   position: 'relative',
-  minWidth: '100%',
-  pl: '10px',
+  flex: '0 0 100%',
 })
 
-const SlideInnerWrap = styled('div', {
-  position: 'relative',
-  overflow: 'hidden',
+const CarouselFooter = styled('div', {
+  display: 'flex',
+  gap: '$8',
+  mt: '$16',
+
+  '@tabletUp': {
+    mt: '$20',
+  },
 })
 
-const DotsWrap = styled('div', {
-  overflow: 'hidden',
+const LiveRegion = styled('div', {
+  visuallyHidden: '',
+})
+
+const Dot = styled('button', {
+  width: 6,
+  height: 6,
+  border: 'none',
+  padding: 0,
+  margin: 0,
+  borderRadius: '50%',
+  backgroundColor: '$grey',
+  cursor: 'pointer',
+
+  '& + &': {
+    ml: '$xxxs',
+  },
+})
+
+const VisuallyHiddenLabel = styled('span', {
+  visuallyHidden: '',
 })
