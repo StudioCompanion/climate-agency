13407c48f9ea280309c012a6b96fc951a30c152e
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-06-21 11:07:38 +0100

    feat: team block (#41)
    * feat: team block
    
    * refactor: team grid
    
    * chore: change svg folder of netzero block
    
    * chore: address PR comments + reorganised media folder

---
+++ src/components/Cards/TeamMember.tsx
@@ -0,0 +1,67 @@
+import { styled } from 'styles/stitches.config'
+
+import { MediaImage, MediaImageProps } from '../Media/MediaImage'
+import { Link, LinkProps } from '../Links/Link'
+
+export interface TeamMemberProps {
+  mediaImage: MediaImageProps
+  name: string
+  role: string
+  description: string
+  socialLink: LinkProps
+}
+
+export const TeamMember = ({
+  mediaImage,
+  name,
+  role,
+  description,
+  socialLink,
+}: TeamMemberProps) => {
+  return (
+    <TeamMemberWrap>
+      <MediaImage {...mediaImage} />
+      <ContentWrap>
+        <NameAndRoleWrap>{name}</NameAndRoleWrap>
+        <NameAndRoleWrap>{role}</NameAndRoleWrap>
+        <DescriptionWrap>{description}</DescriptionWrap>
+        <LinkWrap {...socialLink} />
+      </ContentWrap>
+    </TeamMemberWrap>
+  )
+}
+
+const TeamMemberWrap = styled('section', {
+  display: 'flex',
+  flexDirection: 'column',
+  maxWidth: '343px',
+  gap: '$8',
+})
+
+const ContentWrap = styled('p', {})
+
+const NameAndRoleWrap = styled('span', {
+  display: 'block',
+  fontFamily: '$inria',
+  fontWeight: '$light',
+  fontSize: '$S',
+  lineHeight: '$S',
+})
+
+const DescriptionWrap = styled('span', {
+  display: 'block',
+  whiteSpace: 'pre-line',
+  fontFamily: '$workSans',
+  fontWeight: '$regular',
+  fontSize: '$XXS',
+  lineHeight: '$XXS',
+  my: '$12',
+})
+
+const LinkWrap = styled(Link, {
+  display: 'block',
+  fontWeight: '$regular',
+  lineHeight: '$XXS',
+  textDecoration: 'underline',
+  p: '0',
+})
