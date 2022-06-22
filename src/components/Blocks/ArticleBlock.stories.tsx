2618355023b6a96d5c9aa864d0698d4df22e40f7
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-06-22 15:14:34 +0100

    feat: connect with partners component (#46)
    * feat: connect with partners component
    
    * refactor: article component for stakeholders and partners
    
    * refactor: make icon reusable
    
    Co-authored-by: Josh <37798644+joshuaellis@users.noreply.github.com>

---
+++ src/components/Blocks/ArticleBlock.stories.tsx
@@ -0,0 +1,63 @@
+import { ArticleBlock } from './ArticleBlock'
+import SpikesIcon from '../../assets/Spikes.svg'
+import SunIcon from '../../assets/Sun.svg'
+
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { StoryFixtures } from '../../types/utils'
+
+export default {
+  title: 'Blocks/Article Block',
+  component: ArticleBlock,
+} as ComponentMeta<typeof ArticleBlock>
+
+export const Left: ComponentStory<typeof ArticleBlock> = () => (
+  <ArticleBlock {...fixtures.left} />
+)
+export const Right: ComponentStory<typeof ArticleBlock> = () => (
+  <ArticleBlock {...fixtures.right} />
+)
+
+const fixtures: StoryFixtures<typeof ArticleBlock> = {
+  left: {
+    pageLayout: 'left',
+    children: <SunIcon />,
+    title: `Helping you plan, connect and deliver with great partners`,
+    content: `After the workshop, you may feel you need help building assets (website, video), or running a
+        campaign (fundraising, influencers). We can help.
+        
+        We will either find the right partners to do this for you, negotiate with them to work “at cost”, and help brief them. We are well connected within the agency / creative world and will use our network to help you scale up. Or do it ourselves if we can, charging for our time at cost.`,
+    mediaImage: {
+      image: {
+        src: '/images/people_gathering.jpg',
+        altText: 'People gathering',
+        width: 989,
+        height: 686,
+      },
+      layout: 'responsive',
+      objectFit: 'cover',
+    },
+  },
+  right: {
+    pageLayout: 'right',
+    children: <SpikesIcon />,
+    title: `Stakeholder Campaigns on neglected climate topics`,
+    content: `We believe there are neglected topics which are critical to climate yet poorly understood by key stakeholders such as politicians, philanthropists, CEOs, Media & influencers etc.
+
+    Some stories are screaming to be told and are needed to nudge decisions taken by stakeholders. For example, key actions needed around Methane, or the role of the ocean as a climate solution.
+    
+    Facts and figures are not enough: we can invite some of the best creative and strategic minds to help tell these stories in a way which is persuasive, powerful, and relevant for stakeholders – creating highly influential and targeted campaigns.
+    
+    We are working with founders, scientists and agencies to bring to life these topical stakeholder campaigns.`,
+    mediaImage: {
+      image: {
+        src: '/images/ripples.jpg',
+        altText: 'Ripples in water',
+        width: 541,
+        height: 686,
+      },
+      layout: 'responsive',
+      objectFit: 'cover',
+    },
+  },
+}
---
+++ src/components/Blocks/ArticleBlock.tsx
@@ -0,0 +1,181 @@
+import { ReactNode } from 'react'
+import { styled } from 'styles/stitches.config'
+
+import { MediaImage, MediaImageProps } from '../Media/MediaImage'
+
+interface ArticleBlockProps {
+  pageLayout: 'left' | 'right'
+  children: ReactNode
+  title: string
+  content: string
+  mediaImage: MediaImageProps
+}
+
+export const ArticleBlock = ({
+  pageLayout,
+  children,
+  title,
+  content,
+  mediaImage,
+}: ArticleBlockProps) => {
+  return (
+    <ArticleWrap pageLayout={pageLayout}>
+      <HeaderContainer pageLayout={pageLayout}>
+        <Icon pageLayout={pageLayout}>{children}</Icon>
+        <Title>{title}</Title>
+      </HeaderContainer>
+      <Content pageLayout={pageLayout}>{content}</Content>
+      <ImageContainer pageLayout={pageLayout}>
+        <MediaImage {...mediaImage} />
+      </ImageContainer>
+    </ArticleWrap>
+  )
+}
+
+const ArticleWrap = styled('section', {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(8, 1fr)',
+  columnGap: '$16',
+  px: '$16',
+
+  '@tabletUp': {
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridTemplateRows: 'repeat(auto, 1fr)',
+    columnGap: '$20',
+    px: '$20',
+  },
+
+  variants: {
+    pageLayout: {
+      left: {
+        mb: '$60',
+
+        '@tabletUp': {
+          mb: '$120',
+        },
+      },
+      right: {
+        pt: '$60',
+        pb: '$50',
+
+        '@tabletUp': {
+          pt: '$120',
+          pb: '$100',
+        },
+      },
+    },
+  },
+})
+
+const HeaderContainer = styled('div', {
+  gridColumn: 'span 8',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$20',
+
+  variants: {
+    pageLayout: {
+      left: {
+        '@tabletUp': {
+          flexDirection: 'row',
+          gridColumn: '2 / span 10',
+        },
+      },
+      right: {
+        '@tabletUp': {
+          flexDirection: 'row',
+          gridColumn: '4 / span 9',
+        },
+      },
+    },
+  },
+})
+
+const Icon = styled('div', {
+  '@tabletUp': {
+    alignSelf: 'flex-start',
+    mt: '$40',
+  },
+
+  variants: {
+    pageLayout: {
+      left: {
+        '& > *': {
+          width: '21px',
+
+          '@tabletUp': {
+            width: '36px',
+          },
+        },
+      },
+      right: {
+        '& > *': {
+          width: '24px',
+
+          '@tabletUp': {
+            width: '37px',
+          },
+        },
+      },
+    },
+  },
+})
+
+const Title = styled('h2', {
+  fontFamily: '$inria',
+  fontWeight: '$light',
+  fontSize: '$XL',
+  lineHeight: '$XL',
+
+  '@tabletUp': {
+    fontSize: '$XXXL',
+    lineHeight: '$XXXL',
+  },
+})
+
+const Content = styled('p', {
+  whiteSpace: 'pre-line',
+  fontFamily: '$workSans',
+  fontWeight: '$regular',
+  fontSize: '$XS',
+  lineHeight: '$XS',
+  gridColumn: 'span 8',
+  mt: '$20',
+  mb: '$40',
+
+  variants: {
+    pageLayout: {
+      left: {
+        '@tabletUp': {
+          gridColumn: '5 / span 4',
+          my: '$40',
+        },
+      },
+      right: {
+        '@tabletUp': {
+          gridColumn: '7 / span 4',
+          my: '$40',
+        },
+      },
+    },
+  },
+})
+
+const ImageContainer = styled('div', {
+  gridColumn: 'span 8',
+
+  variants: {
+    pageLayout: {
+      left: {
+        '@tabletUp': {
+          gridColumn: 'span 9',
+        },
+      },
+      right: {
+        '@tabletUp': {
+          gridColumn: '6 / span 5',
+        },
+      },
+    },
+  },
+})
