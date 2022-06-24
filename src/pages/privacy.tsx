a6afae91352c150e0e17f7b0735f3ad266628726
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-06-24 11:48:04 +0100

    feat: privacy page

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,290 @@
+import { styled } from 'styles/stitches.config'
+
+import { Link, LinkProps } from '../components/Links/Link'
+
+const link = {
+  href: `https://twitter.com/ClimateAgency1`,
+  children: `here`,
+  color: 'transparent',
+  isExternal: true,
+} as LinkProps
+
+const Privacy = () => {
+  return (
+    <PrivacyWrap>
+      <Title> Privacy Policy</Title>
+      <ContentWrap>
+        <Subtitle>Introduction</Subtitle>
+        <Paragraph>
+          <SubParagraph>
+            The following terms and conditions (“Terms”) govern your use of,
+            access to, and purchase of products or services from our online and
+            mobile websites, platforms, and applications owned and operated by
+            Seed Health, Inc. (“Seed,” “we,” “us,” or “our”), including without
+            limitation, www.seed.com (collectively, our “Website”). By using our
+            Website, you agree to comply with and be bound by all of the Terms
+            contained below, along with the terms and conditions as stated in
+            our Privacy Policy (please refer to the Privacy Policy section below
+            for more information).
+          </SubParagraph>
+          <SubParagraph>
+            We reserve the right to change these Terms from time to time without
+            notice. The date of the latest version of these Terms is indicated
+            at the end of these Terms. You acknowledge and agree that it is your
+            responsibility to review these Terms periodically to familiarize
+            yourself with any modifications. Your continued use of our Website
+            after such modifications will constitute acknowledgment and
+            agreement of the modified terms and conditions. If you do not wish
+            to be bound by these Terms, do not access our Website and do not
+            purchase our products or services.
+          </SubParagraph>
+          <SubParagraph>
+            PLEASE READ THESE TERMS CAREFULLY BEFORE USING OUR WEBSITE
+          </SubParagraph>
+        </Paragraph>
+        <Subtitle>Responsible Use and Conduct</Subtitle>
+        <Paragraph>
+          <SubParagraph>
+            By visiting our Website and accessing and using the information,
+            resources, services, and tools we provide for you, and purchasing
+            our products or services, either directly or indirectly (our
+            “Resources”), you agree to use these Resources only for the purposes
+            intended as permitted by (a) these Terms, and (b) applicable laws,
+            regulations and generally accepted online practices or guidelines.
+          </SubParagraph>
+          <SubParagraph>You further understand that:</SubParagraph>
+          <SubParagraph>
+            In order to access our Resources, you may be required to provide
+            certain personal information about yourself (such as identification,
+            contact details, etc.) as part of a registration process, as part of
+            your ability to use the Resources, or your ability to buy our
+            products or services. You agree that any information you provide
+            will always be accurate, correct, and up to date.
+          </SubParagraph>
+          <SubParagraph>
+            You are responsible for maintaining the confidentiality of any login
+            information associated with any account you use to access our
+            Resources. Accordingly, you are responsible for all activities that
+            occur under your account(s).
+          </SubParagraph>
+          <SubParagraph>
+            Accessing (or attempting to access) any of our Resources by any
+            means other than through the means we provide, is strictly
+            prohibited. You specifically agree not to access (or attempt to
+            access) any of our Resources through any automated, unethical or
+            unconventional means.
+          </SubParagraph>
+          <SubParagraph>
+            Engaging in any activity that disrupts or interferes with our
+            Resources, including the servers and/or networks to which our
+            Resources are located or connected, is strictly prohibited.
+          </SubParagraph>
+          <SubParagraph>
+            Attempting to copy, duplicate, reproduce, sell, trade, or resell our
+            Resources is strictly prohibited.
+          </SubParagraph>
+          <SubParagraph>
+            You are solely responsible for any consequences, losses, or damages
+            that we may directly or indirectly incur or suffer due to any
+            unauthorized activities conducted by you, as explained above, and
+            may incur criminal or civil liability.
+          </SubParagraph>
+          <SubParagraph>
+            We do not assume any liability for any content posted by you or any
+            other 3rd party users of our Website. However, any content posted by
+            you using any open communication tools on our Website, provided that
+            it doesn’t violate or infringe on any 3rd party copyrights or
+            trademarks, becomes the property of Seed, and as such, gives us a
+            perpetual, irrevocable, worldwide, royalty-free, exclusive license
+            to reproduce, modify, adapt, translate, publish, publicly display
+            and/or distribute as we see fit. This only refers and applies to
+            content posted via open communication tools as described, and does
+            not refer to information that is provided as part of any
+            registration process, necessary in order to use our Resources. All
+            information provided as part of our registration process is covered
+            by our privacy policy.
+          </SubParagraph>
+          <SubParagraph>
+            We may provide various open communication tools on our Website, such
+            as blog comments, blog posts, public chat, forums, message boards,
+            newsgroups, product ratings and reviews, various social media
+            services, etc. (“Your Submissions”). You understand that generally
+            we do not pre-screen or monitor the content posted by users of these
+            various communication tools, which means that if you choose to use
+            these tools to submit any type of content to our Website, then it is
+            your personal responsibility to use these tools in a responsible and
+            ethical manner. By posting information or otherwise using any open
+            communication tools as mentioned, you agree that you will not
+            upload, post, share, or otherwise distribute any content that:
+          </SubParagraph>
+          <SubParagraph>
+            a. Is illegal, threatening, defamatory, abusive, harassing,
+            degrading, intimidating, fraudulent, deceptive, invasive, racist, or
+            contains any type of suggestive, inappropriate, or explicit
+            language;
+          </SubParagraph>
+          <SubParagraph>
+            b. Infringes on any trademark, patent, trade secret, copyright, or
+            other proprietary right of any party;
+          </SubParagraph>
+          <SubParagraph>
+            c. Contains any type of unauthorized or unsolicited advertising; or
+          </SubParagraph>
+          <SubParagraph>
+            d. Impersonates any person or entity, including any Seed employees
+            or representatives.
+          </SubParagraph>
+          <SubParagraph>
+            We have the right at our sole discretion to remove any content that,
+            we feel in our judgment does not comply with these Terms, along with
+            any content that we feel is otherwise offensive, harmful,
+            objectionable, inaccurate, or violates any 3rd party copyrights or
+            trademarks. We are not responsible for any delay or failure in
+            removing such content. If you post content that we choose to remove,
+            you hereby consent to such removal, and consent to waive any claim
+            against us.
+          </SubParagraph>
+          <SubParagraph>
+            When you provide Your Submissions on our Website, you grant to Seed
+            a non-exclusive, royalty-free, fully paid, perpetual, worldwide,
+            irrevocable and fully sub-licensable and transferable (in whole or
+            in part) license under all copyrights, trademarks, privacy and
+            publicity rights you own or control to: (i) use, reproduce,
+            transmit, modify, index, adapt, publish, translate, distribute,
+            display and otherwise exploit such content throughout the world in
+            any media, whether now known or hereafter invented, including for
+            any and all purposes, including commercial or marketing purposes,
+            all without further notice to you, with or without attribution, and
+            without the requirement of any permission from or payment to you or
+            any other person or entity, and (ii) to use your name, persona or
+            likeness alone or in connection with such uses, without any
+            obligation or remuneration to you.
+          </SubParagraph>
+        </Paragraph>
+        <Subtitle>Not Healthcare Advice</Subtitle>
+        <Paragraph>
+          <SubParagraph>
+            Our Website is not intended to provide medical advice, diagnosis or
+            treatment. The Resources, including our products and services,
+            provided on and through our Website, including information that may
+            be provided on our Website directly or via linking to third-party
+            sites by a healthcare or nutrition professional, are provided for
+            informational purposes only. Please consult with your physician or
+            other healthcare professional (collectively, “Healthcare
+            Professional”) regarding any medical or health-related diagnosis or
+            treatment options.
+          </SubParagraph>
+          <SubParagraph>
+            You should always consult with your Healthcare Professional, and
+            carefully read all information provided by the manufacturer of a
+            product and on or in any product label or packaging, before using
+            any medication or nutritional, herbal or homeopathic product; before
+            starting any diet or exercise program; or before adopting any
+            treatment for a health problem. Each person is different, and the
+            way you react to a particular product may be significantly different
+            from the way other people react to such product. You should also
+            consult your Healthcare Professional regarding any interactions
+            between any medication you are currently taking and our products.
+          </SubParagraph>
+          <SubParagraph>
+            Seed employees, consultants and agents may publicly describe their
+            experiences with specific Seed products. Any comments made by such
+            employees, consultants and agents that are not endorsed specifically
+            by Seed are strictly their own personal views made in their
+            personally capacity. These comments are not claims made by Seed nor
+            do they represent the view or position of Seed.
+          </SubParagraph>
+        </Paragraph>
+        <Subtitle>Privacy</Subtitle>
+        <Paragraph>
+          <SubParagraph>
+            Your privacy is very important to us, which is why we’ve created a
+            separate Privacy Policy in order to explain in detail how we
+            collect, use, share, secure, and store your private information. Our
+            Privacy Policy is included under the scope of these Terms. To read
+            our privacy policy in its entirety, click <LinkWrap {...link} />.
+          </SubParagraph>
+        </Paragraph>
+      </ContentWrap>
+    </PrivacyWrap>
+  )
+}
+
+export default Privacy
+
+const PrivacyWrap = styled('section', {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(8, 1fr)',
+  gridTemplateRows: 'repeat(auto, 1fr)',
+  columnGap: '$16',
+  px: '$16',
+  mt: '0',
+
+  '@tabletUp': {
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridTemplateRows: 'repeat(auto, 1fr)',
+    columnGap: '$20',
+    px: '$20',
+  },
+})
+
+const Title = styled('h2', {
+  fontFamily: '$inria',
+  fontWeight: '$light',
+  fontSize: '$XXL',
+  lineHeight: '$XXL',
+  gridColumn: 'span 8',
+  mb: '$60',
+
+  '@tabletUp': {
+    gridColumn: '1 / span 6',
+    pl: '$30',
+    mb: '$110',
+  },
+})
+
+const ContentWrap = styled('div', {
+  gridColumn: 'span 8',
+  mb: '$80',
+
+  '@tabletUp': {
+    gridColumn: '5 / span 4',
+    mb: '$120',
+  },
+})
+
+const Subtitle = styled('h3', {
+  fontFamily: '$inria',
+  fontWeight: '$light',
+  fontSize: '$S',
+  lineHeight: '$S',
+  gridColumn: 'span 8',
+  mb: '$40',
+})
+
+const Paragraph = styled('div', {
+  gridColumn: 'span 8',
+  mb: '$40',
+
+  '@tabletUp': {
+    gridColumn: '5 / span 4',
+  },
+})
+
+const SubParagraph = styled('p', {
+  whiteSpace: 'pre-line',
+  fontFamily: '$workSans',
+  fontWeight: '$regular',
+  fontSize: '$XS',
+  lineHeight: '$XS',
+  mb: '$20',
+})
+
+const LinkWrap = styled(Link, {
+  display: 'inline',
+  fontFamily: '$workSans',
+  fontWeight: '$regular',
+  fontSize: '$XS',
+  lineHeight: '$XS',
+  p: '0',
+})
