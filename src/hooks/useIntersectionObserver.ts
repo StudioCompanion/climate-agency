a0469e4dbbe1935deaefb84e775ac2abf1983eb5
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-06-28 09:51:28 +0100

    feat: add video (#82)
    * feat: add video
    
    * chore: add story

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,27 @@
+import { RefObject, useEffect, useState } from 'react'
+
+export const useIntersection = (
+  ref: RefObject<HTMLElement>,
+  options?: IntersectionObserverInit
+): IntersectionObserverEntry | null => {
+  const [intersectionObserverEntry, setIntersectionObserverEntry] =
+    useState<IntersectionObserverEntry | null>(null)
+
+  useEffect(() => {
+    if (ref.current && typeof IntersectionObserver === 'function') {
+      const handler = (entries: IntersectionObserverEntry[]) => {
+        setIntersectionObserverEntry(entries[0])
+      }
+
+      const observer = new IntersectionObserver(handler, options)
+      observer.observe(ref.current)
+
+      return () => {
+        setIntersectionObserverEntry(null)
+        observer.disconnect()
+      }
+    }
+  }, [options])
+
+  return intersectionObserverEntry
+}
